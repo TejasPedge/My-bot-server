a88d779d13f8ed7f5855664c12a737f959b97ca2
tejaspedge <tejaspedge512@gmail.com>
2023-10-23 15:56:18 +0530

    done

---
+++ index.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+const app = express();
+
+const {Telegraf} = require('telegraf');
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.post('/portal/verify', async (req, res) => {
+
+    try {
+
+        let {chat_id} = req.body;
+        let {group_id} = req.body;
+
+        console.log('chat_id--------->>>', chat_id)
+
+        if (!chat_id || !group_id) return res.status(400).send("chat id & group id is required");
+
+        chat_id = Number(chat_id);
+
+        group_id = Number(group_id);
+
+        console.log('group id -------->>>>',group_id)
+
+        const expireDate = Math.floor(Date.now() / 1000) + 60;
+
+        const generate_invite_link = await bot.telegram.createChatInviteLink(group_id, expireDate, 1); // 1 is the limit of users that can join through the invite link
+
+        // -------------------------Valid Link for one min -------------------------\\
+        setTimeout(async () => {
+            console.log('link expired====>>')
+            await bot.telegram.revokeChatInviteLink(group_id, generate_invite_link?.invite_link);
+        }, 60000);
+        // ---------------------------------------------------------------------------\\
+
+        console.log('generate_invite_link', generate_invite_link);
+
+        const mesage = `Verified, you can join the group using this temporary link:
+
+${generate_invite_link?.invite_link}    
+
+This link is a one time use and will expire`;
+
+        await bot.telegram.sendMessage(chat_id, mesage);
+
+        console.log(chat_id, '===========>>>.chat id');
+
+        console.log('request body-------->>>>>', req.body);
+
+        res.status(200).send("done");
+        console.log('i run----');
+    } catch (error) {
+        console.log('error in verify API post request', error);
+    }
+
+});
+
+
+app.listen(8181, () => {
+    console.log('server is running on port 8181');
+});
+
+
+
