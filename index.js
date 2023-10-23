35f1138ee4f2868596c4f014d6cd06a2048d02f1
tejaspedge <tejaspedge512@gmail.com>
2023-10-23 17:02:24 +0530

    done

--- index.js
+++ index.js
@@ -31,13 +31,13 @@ app.post('/portal/verify', async (req, res) => {
 
         // const expireDate = Math.floor(Date.now() / 1000) + 60;
 
-        const generate_invite_link = await bot.telegram.createChatInviteLink(group_id); // 1 is the limit of users that can join through the invite link
+        const generate_invite_link = await bot.telegram.createChatInviteLink(group_id, null, null, 1); // 1 is the limit of users that can join through the invite link
 
         // -------------------------Valid Link for one min -------------------------\\
-        // setTimeout(async () => {
-        //     console.log('link expired====>>')
-        //     await bot.telegram.revokeChatInviteLink(group_id, generate_invite_link?.invite_link);
-        // }, 60000);
+        setTimeout(async () => {
+            console.log('link expired====>>')
+            await bot.telegram.revokeChatInviteLink(group_id, generate_invite_link?.invite_link);
+        }, 60000);
         // ---------------------------------------------------------------------------\\
 
         console.log('generate_invite_link', generate_invite_link);
